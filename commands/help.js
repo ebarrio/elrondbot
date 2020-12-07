02f8dbfe930c2d81c79be7af5072f25f3a554042
Dan Poage <dan.poage@gmail.com>
2020-12-06 20:13:55 -0600

    Fixed help command.

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,9 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game bear bot - List of commands\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game Bear Bot 3.0.0 - List of commands\n" +
       "!help - This help message\n" +
-      "!hob <query> - Find and display offical card text from Hall of Beorn\n" +
-      "!hobc <query> - Find and display card text from Hall of Beorn (including community)\n" +
-      "!hobimg <query> - Find and display offical card image from Hall of Beorn\n" +
-      "!hobimgc <query> - Find and display card image from Hall of Beorn (including community)\n" +
+      "!hob <query> - Find and display card text from Hall of Beorn\n" +
+      "!hobimg <query> - Find and display card image from Hall of Beorn\n" +
       "!quest - Select a random quest\n" +
       "!hero - Select a random hero\n" +
       "!card - Select a random card (skipping heroes)\n" +
