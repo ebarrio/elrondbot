24075d6fbef1bfaac8aeee3e4d99ec55f41d679f
Dan Poage <dan.poage@gmail.com>
2020-12-16 02:06:32 -0600

    3.0.5: Include ALeP spoilers

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.0.4 (2020-12-14)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.0.5 (2020-12-16)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
