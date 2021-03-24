07350594faf8c6a0f1b0585528fa45d54332dc20
Dan Poage <dan.poage@gmail.com>
2021-03-23 19:00:51 -0500

    3.0.11

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.0.10 (2021-03-22)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.0.11 (2021-03-23)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
