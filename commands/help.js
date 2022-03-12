2e9a566a3c695d05e6787e817dea81d191a3f042
Dan Poage <dan.poage@gmail.com>
2022-03-12 11:45:53 -0600

    3.1.4: Added errata to Starter Deck cards and fixed card numbers.

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.1.3 (2022-03-07)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.1.4 (2022-03-12)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
