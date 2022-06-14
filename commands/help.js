f2160555e602ab4517ccf62259b111f9be3251e1
Dan Poage <dan.poage@gmail.com>
2022-06-13 23:55:15 -0500

    3.2.4: Added Mustering of the Rohirrim

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.2.3 (2022-04-13)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.2.4 (2022-06-13)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
