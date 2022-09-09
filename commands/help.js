7621b29c020bcab4511e87c957dcd296ce07249c
Dan Poage <dan.poage@gmail.com>
2022-09-09 17:11:46 -0500

    3.7.3: Updated Cards.json for Blood in the Isen. Updated Blogs.json data.

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.7.2 (2022-09-08)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.7.3 (2022-09-09)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
