10ae7bcba6d37c7ee2aaf65b98b56d2b21bd30b3
Dan Poage <dan.poage@gmail.com>
2022-10-05 15:29:32 -0500

    3.8.4: More work on day command.

--- commands/cardOfTheDay.js
+++ commands/cardOfTheDay.js
@@ -20,16 +20,14 @@ module.exports = function cardOfTheDay(cardList, emoji, logger, bot) {
         try {
             channel.send(`**Card of the Day**\n`)
               .then(() => channel.send({ files : [`${card.imagesrc}`] }))
-              .then(() => channel.send(helpers.createCardMessage(emoji, card))
-                .then(m => {
-                    m.react('\u0031\ufe0f\u20e3')
-                    .then(m => m.react('\u0032\ufe0f\u20e3'))
-                    .then(m => m.react('\u0033\ufe0f\u20e3'))
-                    .then(m => m.react('\u0034\ufe0f\u20e3'))
-                    .then(m => m.react('\u0035\ufe0f\u20e3'))
-                    .catch(e => console.error('one of the emojis failed to react', e))
-                })
-              );
+              .then(() => channel.send(helpers.createCardMessage(emoji, card)))
+              .then(m => {
+                  m.react('\u0031\ufe0f\u20e3')
+                  m.react('\u0032\ufe0f\u20e3')
+                  m.react('\u0033\ufe0f\u20e3')
+                  m.react('\u0034\ufe0f\u20e3')
+                  m.react('\u0035\ufe0f\u20e3')
+              });
         } catch (err) {
             console.log(err);
             logger.error(err);
