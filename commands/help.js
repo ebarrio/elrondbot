b051c391d1f0068425f42896e72d967238d04e66
Dan Poage <daniel.poage@shipstation.com>
2024-08-24 17:00:54 -0500

    3.9.7: Added data for Ered Mithrin and Dream-chaser Campaign Expansions

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.9.6 (2024-03-03)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.9.7 (2024-08-24)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
