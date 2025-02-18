40b5d3a17b8a22dc92ce86f5ce5265f4b2b915fa
Dan Poage <daniel.poage@auctane.com>
2025-02-17 22:23:14 -0600

    3.9.12: Update Cards.json and Quests.json for Strange News in Bree (ALeP).

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = function help(channel) {
   channel.send(
-    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.9.11 (2024-11-28)\n\n" +
+    "**BeornBot** - Lord of the Rings: The Card Game - Bear Bot 3.9.12 (2025-02-17)\n\n" +
       "List of Commands:\n" +
       "!help - This help message\n" +
       "!hob <query> - Find and display card text from Hall of Beorn\n" +
